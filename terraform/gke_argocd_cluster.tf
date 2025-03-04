99475142c37f1281781cc1ca3e774a694387dad5
ILAN-GiM3L <dreadlord457@gmail.com>
2025-03-04 12:47:44 +0200

    Just Pretend

--- terraform/gke_argocd_cluster.tf
+++ terraform/gke_argocd_cluster.tf
@@ -30,7 +30,7 @@ resource "google_container_node_pool" "argocd_nodes" {
   node_config {
     machine_type = "e2-medium"
     image_type   = "COS_CONTAINERD"
-    disk_size_gb = 5
+    disk_size_gb = 15
     disk_type    = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
--- terraform/gke_cluster.tf
+++ terraform/gke_cluster.tf
@@ -30,7 +30,7 @@ resource "google_container_node_pool" "bleachdle_ephemeral_nodes" {
   node_config {
     machine_type = "e2-medium"
     image_type   = "COS_CONTAINERD"
-    disk_size_gb = 10
+    disk_size_gb = 15
     disk_type    = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
