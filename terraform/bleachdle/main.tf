8b4adb9c1d875c5cd23a0f7b14589ca7ed70be67
ILAN-GiM3L <dreadlord457@gmail.com>
2025-03-10 20:21:25 +0200

    Faint

--- terraform/bleachdle/main.tf
+++ terraform/bleachdle/main.tf
@@ -3,7 +3,6 @@
 ###############################################################################
 terraform {
   backend "gcs" {
-    # Notice a different prefix for the ephemeral state
     bucket = "bleachdle-terraform-state-bucket"
     prefix = "terraform/bleachdle/state"
   }
@@ -35,6 +34,9 @@ provider "google" {
   region  = var.GCP_REGION
 }
 
+##############################
+# KMS
+##############################
 resource "google_kms_key_ring" "vault_key_ring" {
   name     = "vault-key-ring"
   location = var.GCP_REGION
@@ -47,25 +49,124 @@ resource "google_kms_crypto_key" "vault_key" {
   purpose         = "ENCRYPT_DECRYPT"
 }
 
-# 1. Allow the Kubernetes service account (bleachdle-sa) to impersonate terraform-admin service account
-resource "google_iam_policy_binding" "bleachdle_sa_impersonation" {
-  role    = "roles/iam.serviceAccountTokenCreator"
-  members = [
-    "serviceAccount:bleachdle-sa@${var.GCP_PROJECT}.iam.gserviceaccount.com"
-  ]
-  resource = "projects/${var.GCP_PROJECT}/serviceAccounts/terraform-admin@${var.GCP_PROJECT}.iam.gserviceaccount.com"
+##############################
+# GKE: ephemeral cluster
+##############################
+resource "google_container_cluster" "bleachdle_ephemeral" {
+  name                     = "bleachdle-cluster"
+  location                 = var.GCP_REGION
+  node_locations           = ["us-central1-a"]
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = "default"
+  subnetwork = "default"
+
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+  }
+
+  # Enable Workload Identity
+  workload_identity_config {
+    workload_pool = "${var.GCP_PROJECT}.svc.id.goog"
+  }
 }
 
-# 2. Grant KMS Crypto Key Encrypter/Decrypter permissions to the Kubernetes service account (bleachdle-sa)
+resource "google_container_node_pool" "bleachdle_ephemeral_nodes" {
+  name     = "bleachdle-ephemeral-nodes"
+  cluster  = google_container_cluster.bleachdle_ephemeral.name
+  location = var.GCP_REGION
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    machine_type = "e2-medium"
+    image_type   = "COS_CONTAINERD"
+    disk_size_gb = 15
+    disk_type    = "pd-standard"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  management {
+    auto_upgrade = true
+    auto_repair  = true
+  }
+}
+
+##############################
+# Workload Identity Binding
+##############################
+# This allows your K8s SA (in the default namespace) to act as
+# terraform-admin@bleachdle-web.iam.gserviceaccount.com
+# by granting roles/iam.workloadIdentityUser on that GCP SA.
+#
+# NOTE: The "member" is set to:
+#   "serviceAccount:<PROJECT>.svc.id.goog[<NAMESPACE>/<K8S_SA>]"
+# ...matching the annotation we added to the SA in the Helm chart.
+##############################
+
+resource "google_service_account_iam_binding" "allow_bleachdle_sa_impersonation" {
+  service_account_id = "terraform-admin@${var.GCP_PROJECT}.iam.gserviceaccount.com"
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "serviceAccount:${var.GCP_PROJECT}.svc.id.goog[default/bleachdle-sa]",
+  ]
+}
+
+##############################
+# KMS IAM: let terraform-admin@... do KMS
+##############################
+# If you also want the same GCP SA to have wide KMS perms:
+# you already have it as an Owner, but here’s an example if you want a more minimal approach:
 resource "google_kms_crypto_key_iam_member" "bleachdle_sa_kms_permissions" {
   crypto_key_id = google_kms_crypto_key.vault_key.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:bleachdle-sa@${var.GCP_PROJECT}.iam.gserviceaccount.com"
+  member        = "serviceAccount:terraform-admin@${var.GCP_PROJECT}.iam.gserviceaccount.com"
 }
 
+# Optional: If you want even broader KMS admin capabilities:
+# resource "google_kms_crypto_key_iam_member" "bleachdle_sa_kms_admin_permissions" {
+#   crypto_key_id = google_kms_crypto_key.vault_key.id
+#   role          = "roles/cloudkms.admin"
+#   member        = "serviceAccount:terraform-admin@${var.GCP_PROJECT}.iam.gserviceaccount.com"
+# }
 
+##############################
+# K8S & HELM providers
+##############################
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.bleachdle_ephemeral.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.bleachdle_ephemeral.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
 
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.bleachdle_ephemeral.endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.bleachdle_ephemeral.master_auth[0].cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
+}
 
 data "google_client_config" "default" {}
 
+###############################################################################
+# Outputs
+###############################################################################
+output "bleachdle_cluster_endpoint" {
+  value = google_container_cluster.bleachdle_ephemeral.endpoint
+}
 
+output "bleachdle_cluster_name" {
+  value = google_container_cluster.bleachdle_ephemeral.name
+}
